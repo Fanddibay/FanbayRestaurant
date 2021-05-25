d86f4817a2057db15d178b2e432fc7f54848ba26
Fanbayy <fandi.bayu110@gmail.com>
2021-05-25 19:43:54 +0700

    menambahkan dark theme

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -14,3 +14,23 @@ mainElement.addEventListener("click", (event) => {
   drawerElement.classList.remove("open");
   event.stopPropagation();
 });
+
+const buttonDarkTheme = document.querySelector("#darkTheme");
+const mainDarkTheme = document.querySelector("main");
+const ourShop = document.querySelector("#explore__shop");
+const mainTitle = document.querySelector("#main__h2");
+const menuShop = document.querySelector("#about");
+const drawer = document.querySelector("#drawer");
+const header = document.querySelector("#header__logo");
+const body = document.querySelector("body");
+
+buttonDarkTheme.addEventListener("click", (event) => {
+  mainDarkTheme.classList.toggle("close");
+  ourShop.classList.toggle("explore__menu");
+  mainTitle.classList.toggle("__h1");
+  menuShop.classList.toggle("menuShop__dark");
+  drawer.classList.toggle("explore__menu");
+  header.classList.toggle("explore__menu");
+  body.classList.toggle("menuShop__dark");
+  event.stopPropagation();
+});
